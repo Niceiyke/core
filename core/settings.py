6a11093bafe9f7646c24e79ee4672e8b4ccc8dda
Niceiyke <74918186+Niceiyke@users.noreply.github.com>
2022-07-26 13:13:01 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1nmvg=wafl3h*5x+7zzo*(2e$ma@)61@3c_pdv3mkrczd**kaw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['djangopredict.herokuapp.com']
 
