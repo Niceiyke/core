e5a9d96c0d3fbb7ca92f2bd182095a1a158c7971
Niceiyke <74918186+Niceiyke@users.noreply.github.com>
2022-07-27 12:33:36 +0100

    Update tasks.py

--- scrape/tasks.py
+++ scrape/tasks.py
@@ -11,12 +11,14 @@ from celery import shared_task
 from .models import Prediction
 from scipy.stats import poisson
 import numpy as np
+import os
 
-
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(service=Service(
-    ChromeDriverManager().install()), options=options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
 @shared_task
